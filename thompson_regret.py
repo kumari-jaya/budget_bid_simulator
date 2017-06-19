816263fc170b73274a834dba5a65a2e75237676b
Guglielmo Acabbi <gugohb@hotmail.it>
2017-06-19 16:58:31 +0200

    Aggiornato thompson per usare n campagne

--- thompson_regret.py
+++ thompson_regret.py
@@ -22,7 +22,7 @@ a4= Auction(nbidders=5 , nslots=5, mu=0.57 , sigma=0.39, lambdas=lambdas)
 
 
 ncampaigns=5
-c1 = Campaign(a1, nusers=1000.0 , probClick=0.1 ,convParams= convparams)
+c1 = Campaign(a1, nusers=1000.0 , probClick=0.5 ,convParams= convparams)
 c2 = Campaign(a2, nusers=1500.0 , probClick=0.6 ,convParams= convparams)
 c3 = Campaign(a3, nusers=1500.0 , probClick=0.6 ,convParams= convparams)
 c4 = Campaign(a2, nusers=1000.0 , probClick=0.5 ,convParams= convparams)
@@ -30,11 +30,11 @@ c5 = Campaign(a4, nusers=1250.0 , probClick=0.4 ,convParams= convparams)
 
 
 env = Environment([c1,c2,c3,c4,c5])
-nBids=7
-nIntervals=5
-deadline = 100
-maxBudget = 200
-agent = Agent(1000, deadline, 2,nIntervals,nBids,maxBudget)
+nBids=5
+nIntervals=9
+deadline = 150
+maxBudget = 100
+agent = Agent(1000, deadline, ncampaigns,nIntervals,nBids,maxBudget)
 agent.initGPs()
 plotter = Plotter(agent=agent,env=env)
 
@@ -51,36 +51,39 @@ for i in range(0,ncampaigns):
 
 
 [newBudgets,newCampaigns] = agent.optimize(optMatrix)
-"""
 # ora ricerco nelle matrici originali il numero di click nell'allocazione ottima
-indexes = np.array([np.argwhere(np.isclose(agent.budgets, newBudgets[0])), np.argwhere(np.isclose(agent.budgets, newBudgets[1]))])
-optValue = (matrix0[indexes[0],:].max() + matrix1[indexes[1],:].max()) * convparams[0]
+optValue = 0
+for i in range(0,ncampaigns):
+    index = np.argwhere(np.isclose(agent.budgets,newBudgets[i]))
+    tempValue = listMatrices[i][index,:].max()
+    optValue += tempValue
+optValue = optValue * convparams[0]  #converto i click in conversioni
 ## questo è il valore dell'oracolo per il plot ora devo simulare i valori del thompson!
-nexperiments = 10
+nexperiments = 3
 # mi salvo le tre realizzazioni degli esperimenti e poi alla fine le medio!
 matrixValues = np.zeros((nexperiments,deadline))
 matrixEst = np.zeros((nexperiments,deadline))
 for k in range(0,nexperiments):
     print "Experiment: ",k+1
-    agent = Agent(1000, deadline, 2,nIntervals,nBids,maxBudget)
+    agent = Agent(1000, deadline, ncampaigns,nIntervals,nBids,maxBudget)
     agent.initGPs()
     core = Core(agent, env, deadline)
     chosenValues = np.zeros((deadline))
     estValues = np.zeros((deadline))
     for t in range(0,deadline):
-        print "Day : ",t+1
+        print "Day: ",t+1
         core.step()
         lastBudgets = agent.prevBudgets[-1,:]
         lastBids = agent.prevBids[-1,:]
-        indBud = np.array([np.argwhere(np.isclose(agent.budgets, lastBudgets[0])), np.argwhere(np.isclose(agent.budgets, lastBudgets[1]))])
-        indBid = np.array([np.argwhere(np.isclose(agent.bids, lastBids[0])), np.argwhere(np.isclose(agent.bids, lastBids[1]))])
-        # il valore dei click medio relativi ai bid e budget scelti li prendo dalle matrici dell'oracolo!
-        chosenValues[t] = (matrix0[indBud[0],indBid[0]] + matrix1[indBud[1],indBid[1]]) * convparams[0]
-        estValues[t] = agent.campaignsValues[0,indBud[0]] + agent.campaignsValues[1,indBud[1]]
+        for i in range(0,ncampaigns):
+            indBud = np.argwhere(np.isclose(agent.budgets, lastBudgets[i]))
+            indBid = np.argwhere(np.isclose(agent.bids, lastBids[i]))
+            # il valore dei click medio relativi ai bid e budget scelti li prendo dalle matrici dell'oracolo!
+            chosenValues[t] += listMatrices[i][indBud,indBid] *convparams[0]
+            estValues[t] += agent.campaignsValues[i,indBud]
     matrixValues[k,:] = chosenValues
     matrixEst [k,:] = estValues
 
 finalValues = matrixValues.mean(axis=0)
 finalEst = matrixEst.mean(axis=0)
 plotter.performancePlot(optValue,finalValues,finalEst)
-"""
